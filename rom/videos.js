aba7205b965943b4b2b34e0244215cafa2da8ecd
Marksportsmassage <fofo88119@gmail.com>
2026-06-17 17:25:59 +0800

    Update videos.js

--- rom/videos.js
+++ rom/videos.js
@@ -1,87 +1,87 @@
 // ROM 影片連結填在這個檔案。
 window.ROM_VIDEO_LINKS = {
   "1": {
-    "url": "",
-    "start": "",
-    "end": "",
-    "note": ""
+    "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
+    "start": "00:10",
+    "end": "02:22",
+    "note": "Endfeel:soft/firm Center:GT 近端:骨盆外側中線 遠端:股骨外上髁（中線）"
   },
   "2": {
-    "url": "",
+    "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "3": {
-    "url": "",
+    "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "4": {
-    "url": "",
+    "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "5": {
-    "url": "",
+    "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "6": {
-    "url": "",
+    "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "7": {
-    "url": "",
+    "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "8": {
-    "url": "",
+    "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "9": {
-    "url": "",
+    "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "10": {
-    "url": "",
+    "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "11": {
-    "url": "",
+    "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "12": {
-    "url": "",
+    "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "13": {
-    "url": "",
+    "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   },
   "14": {
-    "url": "",
+    "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
     "start": "",
     "end": "",
-    "note": ""
+    "note": "Endfeel: Center: 近端: 遠端:"
   }
 };
