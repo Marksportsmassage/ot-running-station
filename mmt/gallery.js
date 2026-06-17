41d31bc1a881aef084df06cfb692958e74f3ff2d
Marksportsmassage <fofo88119@gmail.com>
2026-06-17 16:57:08 +0800

    Add files via upload

---
+++ mmt/gallery.js
@@ -0,0 +1,74 @@
+window.MMT_GALLERY = [
+  {
+    "label": "MMT p.1",
+    "src": "assets/mmt-p01.jpg"
+  },
+  {
+    "label": "MMT p.2",
+    "src": "assets/mmt-p02.jpg"
+  },
+  {
+    "label": "MMT p.3",
+    "src": "assets/mmt-p03.jpg"
+  },
+  {
+    "label": "MMT p.4",
+    "src": "assets/mmt-p04.jpg"
+  },
+  {
+    "label": "MMT p.5",
+    "src": "assets/mmt-p05.jpg"
+  },
+  {
+    "label": "MMT p.6",
+    "src": "assets/mmt-p06.jpg"
+  },
+  {
+    "label": "MMT p.7",
+    "src": "assets/mmt-p07.jpg"
+  },
+  {
+    "label": "MMT p.8",
+    "src": "assets/mmt-p08.jpg"
+  },
+  {
+    "label": "MMT p.9",
+    "src": "assets/mmt-p09.jpg"
+  },
+  {
+    "label": "MMT p.10",
+    "src": "assets/mmt-p10.jpg"
+  },
+  {
+    "label": "MMT p.11",
+    "src": "assets/mmt-p11.jpg"
+  },
+  {
+    "label": "MMT p.12",
+    "src": "assets/mmt-p12.jpg"
+  },
+  {
+    "label": "MMT p.13",
+    "src": "assets/mmt-p13.jpg"
+  },
+  {
+    "label": "MMT p.14",
+    "src": "assets/mmt-p14.jpg"
+  },
+  {
+    "label": "MMT p.15",
+    "src": "assets/mmt-p15.jpg"
+  },
+  {
+    "label": "MMT p.16",
+    "src": "assets/mmt-p16.jpg"
+  },
+  {
+    "label": "MMT p.17",
+    "src": "assets/mmt-p17.jpg"
+  },
+  {
+    "label": "MMT p.18",
+    "src": "assets/mmt-p18.jpg"
+  }
+];
---
+++ rom/gallery.js
@@ -0,0 +1,58 @@
+window.ROM_GALLERY = [
+  {
+    "label": "ROM測量指引 p.1",
+    "src": "assets/guide-p01.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.2",
+    "src": "assets/guide-p02.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.3",
+    "src": "assets/guide-p03.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.4",
+    "src": "assets/guide-p04.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.5",
+    "src": "assets/guide-p05.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.6",
+    "src": "assets/guide-p06.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.7",
+    "src": "assets/guide-p07.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.8",
+    "src": "assets/guide-p08.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.9",
+    "src": "assets/guide-p09.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.10",
+    "src": "assets/guide-p10.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.11",
+    "src": "assets/guide-p11.jpg"
+  },
+  {
+    "label": "ROM測量指引 p.12",
+    "src": "assets/guide-p12.jpg"
+  },
+  {
+    "label": "ROM考題 p.1",
+    "src": "assets/exam-p01.jpg"
+  },
+  {
+    "label": "ROM考題 p.2",
+    "src": "assets/exam-p02.jpg"
+  }
+];
