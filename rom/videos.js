354b948926b219d510360fc74ae558c5392fa5ce
Marksportsmassage <fofo88119@gmail.com>
2026-06-18 12:33:54 +0800

    Update videos.js

--- rom/videos.js
+++ rom/videos.js
@@ -8,33 +8,33 @@ window.ROM_VIDEO_LINKS = {
   },
   "2": {
     "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "02:35",
+    "end": "06:12",
+    "note": "Endfeel:firm  Center:ASIS 近端:兩側ASIS中線 遠端:髕骨,股骨前側線,超出床外需用膝蓋幫他支撐"
   },
   "3": {
     "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "06:13",
+    "end": "07:01",
+    "note": "Endfeel:firm Center:ASIS 近端:兩側ASIS中線 遠端:髕骨,股骨前側線,將對側腳垂出床外"
   },
   "4": {
     "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "07:12",
+    "end": "09:40",
+    "note": "Endfeel: Center: 近端: 遠端:  ,膝蓋處要墊毛巾"
   },
   "5": {
     "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "09:40",
+    "end": "10:08",
+    "note": "Endfeel: Center: 近端: 遠端:,對側腳需外移或是扣住床"
   },
   "6": {
     "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "12:00",
+    "end": "13:40",
+    "note": "Endfeel:soft/firm  Center:股骨外上髁 近端:GT,股骨外側中線 遠端:腓骨外上髁,腓骨外側中線"
   },
   "7": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
