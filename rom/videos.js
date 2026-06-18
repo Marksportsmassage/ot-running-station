8decb5e7ae6cab59b01029935edf5de4ca558130
Marksportsmassage <fofo88119@gmail.com>
2026-06-18 23:15:13 +0800

    Update videos.js

--- rom/videos.js
+++ rom/videos.js
@@ -22,66 +22,66 @@ window.ROM_VIDEO_LINKS = {
     "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
     "start": "07:12",
     "end": "09:40",
-    "note": "Endfeel: Center: 近端: 遠端:  ,膝蓋處要墊毛巾"
+    "note": "Endfeel:firm,Center:髕骨中心點,近端:垂直地板,遠端:小腿前側中線  ,膝蓋處要墊毛巾"
   },
   "5": {
     "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
     "start": "09:40",
     "end": "10:08",
-    "note": "Endfeel: Center: 近端: 遠端:,對側腳需外移或是扣住床"
+    "note": "Endfeel:firm,Center:髕骨中心點,近端:垂直地板,遠端:小腿前側中線,對側腳需外移或是扣住床"
   },
   "6": {
     "url": "https://youtu.be/e8fXDqu3bcU?si=ClJwfkLPZb-Dw7YJ",
     "start": "12:00",
     "end": "13:40",
-    "note": "Endfeel:soft/firm  Center:股骨外上髁 近端:GT,股骨外側中線 遠端:腓骨外上髁,腓骨外側中線"
+    "note": "Endfeel:soft/firm,Center:股骨外上髁,近端:GT,股骨外側中線,遠端:腓骨外上髁,腓骨外側中線"
   },
   "7": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "00:00",
+    "end": "02:10",
+    "note": "Endfeel:firm,Center:外踝,近端:腓骨外側中線、腓骨頭,遠端:第五蹠骨外側中線"
   },
   "8": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "02:10",
+    "end": "02:57",
+    "note": "Endfeel:firm,Center:外踝,近端:腓骨外側中線、腓骨頭,遠端:第五蹠骨外側中線"
   },
   "9": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "02:58",
+    "end": "05:27",
+    "note": "Endfeel:firm,Center:內外髁中心點,近端:小腿前側中線,遠端:第二蹠骨"
   },
   "10": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "02:58",
+    "end": "05:27",
+    "note": "Endfeel:firm,Center:內外髁中心點,近端:小腿前側中線,遠端:第二蹠骨"
   },
   "11": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "05:30",
+    "end": "07:33",
+    "note": "Endfeel:firm,Center:阿基里斯腱中間(內髁與外髁中心),近端:小腿中線(後側),遠端:跟骨後方中線"
   },
   "12": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "05:30",
+    "end": "07:33",
+    "note": "Endfeel:firm,Center:阿基里斯腱中間(內髁與外髁中心),近端:小腿中線(後側),遠端:跟骨後方中線"
   },
   "13": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "07:35",
+    "end": "09:55",
+    "note": "Endfeel:firm, Center:第五蹠骨外側 近端:平行小腿前側中線 遠端:對齊第一到第五蹠骨的蹠側"
   },
   "14": {
     "url": "https://youtu.be/3UOUkaEIzoQ?si=HMkC9O3VwnLWXQe_",
-    "start": "",
-    "end": "",
-    "note": "Endfeel: Center: 近端: 遠端:"
+    "start": "07:35",
+    "end": "09:55",
+    "note": "Endfeel: Center:第一蹠骨內側 近端:平行小腿前側中線 遠端:對齊第一到第五蹠骨的蹠側()"
   }
 };
