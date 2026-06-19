aa0ef48cee63caeb22c4ef6977e7ade82804b316
Marksportsmassage <fofo88119@gmail.com>
2026-06-19 12:10:30 +0800

    Update videos.js

--- mmt/videos.js
+++ mmt/videos.js
@@ -5,15 +5,35 @@
 
 window.MMT_VIDEO_LINKS = {
   "1": {
-    "url": "",
-    "start": "",
-    "end": "",
-    "note": ""
+    "url": "https://youtu.be/aGRAx7yDZ_k?si=fJ0tscujNvx0LODc",
+    "start": "00:10",
+    "end": "02:10",
+    "note": "腕關節
+橈側腕屈肌Flexor carpi radialis 
+尺側腕屈肌Flexor carpi ulnaris
+
+3分  手放在床上桌上 兩條一起測是直接中立位屈曲 還有橈側和尺側偏移並各屈曲一次 能完成full range
+
+5分4分
+一起測：手腕中立位 並微微的伸直 阻力給在手掌 猛5次4
+
+橈側和尺側也側：
+橈偏 微伸直 阻力給在第一第二掌骨（橈側）
+
+尺偏 微伸直  阻力給第五掌骨 （尺側）
+猛5次4
+
+2分 坐姿 尺側朝下 幫患者扶著腕關節近端處 讓患者自己屈曲手腕 一樣可以三個面向都做
+
+1分  前臂旋後 掌心朝上 做屈曲 可注意或摸到肌腱收縮
+
+0分 無注意到收縮
+"
   },
   "2": {
-    "url": "",
-    "start": "",
-    "end": "",
+    "url": "https://youtu.be/aGRAx7yDZ_k?si=fJ0tscujNvx0LODc",
+    "start": "02:11",
+    "end": "04:30",
     "note": ""
   },
   "3": {
